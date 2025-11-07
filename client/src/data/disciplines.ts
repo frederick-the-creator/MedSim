3f6c752a0dbcaf81eb92d27191738e554141776a
Frederick Lewis <frederickl@hotmail.co.uk>
2025-11-07 12:22:29 +0000

    Re-order specialties to put Opthalmology at the top

--- client/src/data/disciplines.ts
+++ client/src/data/disciplines.ts
@@ -5,21 +5,59 @@ export interface Discipline {
 }
 
 export const disciplines: Discipline[] = [
-	{ name: "Internal Medicine Training", isPrimary: false, ctaLabel: "Register interest" },
-	{ name: "Core Surgical Training", isPrimary: false, ctaLabel: "Register interest" },
-	{ name: "Anaesthetics CT1 / ACCS", isPrimary: false, ctaLabel: "Register interest" },
-	{ name: "Cardiothoracic Surgery ST1", isPrimary: false, ctaLabel: "Register interest" },
-	{ name: "CSRH ST1", isPrimary: false, ctaLabel: "Register interest" },
-	{ name: "Emergency Medicine ACCS", isPrimary: false, ctaLabel: "Register interest" },
-	{ name: "GP Selection Centre", isPrimary: false, ctaLabel: "Register interest" },
-	{ name: "Histopathology ST1", isPrimary: false, ctaLabel: "Register interest" },
-	{ name: "Neurosurgery ST1", isPrimary: false, ctaLabel: "Register interest" },
-	{ name: "Obs and Gynae ST1", isPrimary: false, ctaLabel: "Register interest" },
 	{ name: "Ophthalmology ST1", isPrimary: true, ctaLabel: "Sign up" },
+	{
+		name: "Internal Medicine Training",
+		isPrimary: false,
+		ctaLabel: "Register interest",
+	},
+	{
+		name: "Core Surgical Training",
+		isPrimary: false,
+		ctaLabel: "Register interest",
+	},
+	{
+		name: "Anaesthetics CT1 / ACCS",
+		isPrimary: false,
+		ctaLabel: "Register interest",
+	},
+	{
+		name: "Cardiothoracic Surgery ST1",
+		isPrimary: false,
+		ctaLabel: "Register interest",
+	},
+	{ name: "CSRH ST1", isPrimary: false, ctaLabel: "Register interest" },
+	{
+		name: "Emergency Medicine ACCS",
+		isPrimary: false,
+		ctaLabel: "Register interest",
+	},
+	{
+		name: "GP Selection Centre",
+		isPrimary: false,
+		ctaLabel: "Register interest",
+	},
+	{
+		name: "Histopathology ST1",
+		isPrimary: false,
+		ctaLabel: "Register interest",
+	},
+	{ name: "Neurosurgery ST1", isPrimary: false, ctaLabel: "Register interest" },
+	{
+		name: "Obs and Gynae ST1",
+		isPrimary: false,
+		ctaLabel: "Register interest",
+	},
 	{ name: "Paediatrics ST1", isPrimary: false, ctaLabel: "Register interest" },
-	{ name: "Public Health Medicine ST1", isPrimary: false, ctaLabel: "Register interest" },
+	{
+		name: "Public Health Medicine ST1",
+		isPrimary: false,
+		ctaLabel: "Register interest",
+	},
 	{ name: "Radiology ST1", isPrimary: false, ctaLabel: "Register interest" },
-	{ name: "Academic Clinical Fellowship", isPrimary: false, ctaLabel: "Register interest" },
+	{
+		name: "Academic Clinical Fellowship",
+		isPrimary: false,
+		ctaLabel: "Register interest",
+	},
 ];
-
-
